4d3f24a8e434c52935c3e9cd8ff762257d3c7c64
eN1nja <mr.buntar@gmail.com>
2023-12-21 13:39:59 +0300

    создана вторая версия кода с уже приведенными примерами

--- this_code.py
+++ this_code.py
@@ -8,3 +8,23 @@ def filter_strings(arr):
 arr = input("Введите элементы массива через запятую: ").split(",")
 filtered_arr = filter_strings(arr)
 print(filtered_arr)
+
+# def filter_strings(arr):
+#     new_arr = []
+#     for string in arr:
+#         if len(string) <= 3:
+#             new_arr.append(string)
+#     return new_arr
+
+# arr1 = ["Hello", "2", "world", ":-)"]
+# arr2 = ["1234", "1567", "-2", "computer science"]
+# arr3 = ["Russia", "Denmark", "Kazan"]
+
+# filtered_arr1 = filter_strings(arr1)
+# print(filtered_arr1)  # Вывод: ["2", ":-)"]
+
+# filtered_arr2 = filter_strings(arr2)
+# print(filtered_arr2)  # Вывод: ["-2"]
+
+# filtered_arr3 = filter_strings(arr3)
+# print(filtered_arr3)  # Вывод: []
