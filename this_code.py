aed157e80a313a87aa53b3299244ea87ecb7d8f1
eN1nja <mr.buntar@gmail.com>
2023-12-21 13:38:14 +0300

    создан код на python

---
+++ this_code.py
@@ -0,0 +1,10 @@
+def filter_strings(arr):
+    new_arr = []
+    for string in arr:
+        if len(string) <= 3:
+            new_arr.append(string)
+    return new_arr
+
+arr = input("Введите элементы массива через запятую: ").split(",")
+filtered_arr = filter_strings(arr)
+print(filtered_arr)
